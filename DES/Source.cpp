18925d849105c9a39394153ad4d9e78ca01a2934
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-11-17 05:37:12 +0200

    list of keys generation

--- DES/Source.cpp
+++ DES/Source.cpp
@@ -75,9 +75,10 @@ string hexa_to_binary(string in)
 string binary_to_hexa(string in)
 {
 	string out;
-	for (int i = 0;i < in.length();i++)
+	int n = in.length();
+	for (int i = 0;i < n;i+=4)
 	{
-		if (in.substr(i,4) == "1111")
+		if (in.substr(i, 4) == "1111")
 			out += "F";
 		else if (in.substr(i, 4) == "1110")
 			out += "E";
@@ -109,6 +110,8 @@ string binary_to_hexa(string in)
 			out += "1";
 		else if (in.substr(i, 4) == "0000")
 			out += "0";
+		else break;
+
 
 	}
 
@@ -128,14 +131,29 @@ string permution(string text, int n, int a[])
 
 }
 
+string shifting(string key , int shiftBy)
+{
+	string test = "";
+	for (int i = 0; i < shiftBy;i++)
+	{
+		for (int j = 1; j < 28;j++)
+		{
+			test += key[j];
+		}
+		test += key[0];
+		key = test;
+		test = "";
+	}
+	return key;
+}
+
 void key_generate(string key)
 {
 	string test ;
-	/*test = "";
-	for (int i = 0; i < key.length();i += 8)
-		test += key.substr(i, 7);
-		*/
-	int table1[56]{
+	string left_key;
+	string right_key;
+
+	int Parity_drop[56]{
 	57,49,41,33,25,17,9,
 	1,58,50,42,34,26,18,
 	10,2,59,51,43,35,27,
@@ -145,8 +163,44 @@ void key_generate(string key)
 	14,6,61,53,45,37,29,
 	21,13,5,28,20,12,4 };
 
-	key = permution(key,56,table1);
+	int Key_compression[48]{
+	 14, 17, 11, 24, 1, 5,
+	 3, 28, 15, 6, 21, 10,					
+	 23, 19, 12, 4, 26, 8,					
+	 16, 7, 27, 20, 13, 2,					
+	 41, 52, 31, 37, 47, 55,					
+	 30, 40, 51, 45, 33, 48,
+	 44, 49, 39, 56, 34, 53,					
+	 46, 42, 50, 36, 29, 32						 				
+	};
+
+	int shift_by[16]{
+	1,1,2,2,
+	2,2,2,2,
+	1,2,2,2,
+	2,2,2,1	};
+
+
+	key = permution(key,56,Parity_drop);
 	
+	left_key = key.substr(0, 28);
+	right_key = key.substr(28, 28);
+
+	
+
+	for (int r = 0;r < 16;r++)
+	{
+		left_key = shifting(left_key, shift_by[r]);
+		right_key = shifting(right_key, shift_by[r]);
+		keys[r] = left_key + right_key;
+		
+		keys[r] = permution(keys[r], 48, Key_compression);
+
+		//keys[r] = binary_to_hexa(keys[r]);
+		//cout << keys[r]<<endl;
+	}
+
+
 	
 	
 	
