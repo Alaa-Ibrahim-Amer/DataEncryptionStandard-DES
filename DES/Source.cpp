2f923a76fa509858ef76836f9cf99740c2288ba7
Alaa-Ibrahim-Amer <69719564+Alaa-Ibrahim-Amer@users.noreply.github.com>
2021-12-05 02:33:42 +0200

    done

--- DES/Source.cpp
+++ DES/Source.cpp
@@ -3,9 +3,20 @@
 #include<string>
 #include<vector>
 using namespace std;
+
+#ifdef __GNUC__
+# define __rdtsc __builtin_ia32_rdtsc
+#else
+# include<intrin.h>
+#endif
+
+typedef unsigned long long u64;
+
+
 string keys[16];
-string Left_p; //left plane text
-string Right_p; //right plane text
+u64 keys_64[16];
+u64 Left_p; //left plane text
+u64 Right_p; //right plane text
 
 
 
@@ -75,52 +86,7 @@ string hexa_to_binary(string in)
 	return out;
 }
 
-string binary_to_hexa(string in)
-{
-	string out;
-	int n = in.length();
-	for (int i = 0;i < n;i+=4)
-	{
-		if (in.substr(i, 4) == "1111")
-			out += "F";
-		else if (in.substr(i, 4) == "1110")
-			out += "E";
-		else if (in.substr(i, 4) == "1101")
-			out += "D";
-		else if (in.substr(i, 4) == "1100")
-			out += "C";
-		else if (in.substr(i, 4) == "1011")
-			out += "B";
-		else if (in.substr(i, 4) == "1010")
-			out += "A";
-		else if (in.substr(i, 4) == "1001")
-			out += "9";
-		else if (in.substr(i, 4) == "1000")
-			out += "8";
-		else if (in.substr(i, 4) == "0111")
-			out += "7";
-		else if (in.substr(i, 4) == "0110")
-			out += "6";
-		else if (in.substr(i, 4) == "0101")
-			out += "5";
-		else if (in.substr(i, 4) == "0100")
-			out += "4";
-		else if (in.substr(i, 4) == "0011")
-			out += "3";
-		else if (in.substr(i, 4) == "0010")
-			out += "2";
-		else if (in.substr(i, 4) == "0001")
-			out += "1";
-		else if (in.substr(i, 4) == "0000")
-			out += "0";
-		else break;
-
-
-	}
-
-	return out;
-}
-
+//for the key its slow function as they use strings
 string permution(string text, int n, int a[])
 {
 	string test = text;
@@ -198,9 +164,8 @@ void key_generate(string key)
 		keys[r] = left_key + right_key;
 		
 		keys[r] = permution(keys[r], 48, Key_compression);
-
-		//keys[r] = binary_to_hexa(keys[r]);
-		//cout << keys[r]<<endl;
+		keys_64[r] = stoull(keys[r], nullptr, 2);
+		
 	}
 
 
@@ -209,6 +174,7 @@ void key_generate(string key)
 	
 }
 
+//for decryption
 void key_generate_(string key)
 {
 	string test;
@@ -259,14 +225,14 @@ void key_generate_(string key)
 
 		keys[r] = permution(keys[r], 48, Key_compression);
 		keys_ [r]= keys[r];
-		//keys[r] = binary_to_hexa(keys[r]);
-		//cout << keys[r]<<endl;
+		
 	}
 	
 	for (int r = 0;r < 16;r++)
 	{
 		
 		keys[r] = keys_[15 - r];
+		keys_64[r] = stoull(keys[r], nullptr, 2);
 
 	}
 
@@ -275,20 +241,17 @@ void key_generate_(string key)
 
 }
 
-string XOR(string a , string b) {
-	string x = "";
-	for (int i = 0;i < a.length();i++)
-	{
-		if (a[i] == b[i])
-			x += "0";
-		else 
-			x += "1";
 
-	}
+u64 permution(u64 in, int n, int a[],int m)
+{
+	u64 out = 0;
+	for (int i = 0;i < n;++i)
 
-	return x;
+		out |= (in >> (m - a[n - 1 - i]) & 1) << i;
+	return out;
 }
-string ED_box(string right , string key) {
+
+u64 ED_box(u64 right , u64 key) {
 	int ED [48] = { 
 	32, 1, 2, 3, 4, 5, 4, 5,
 	6, 7, 8, 9, 8, 9, 10, 11,
@@ -296,13 +259,15 @@ string ED_box(string right , string key) {
 	16, 17, 18, 19, 20, 21, 20, 21,
 	22, 23, 24, 25, 24, 25, 26, 27,
 	28, 29, 28, 29, 30, 31, 32, 1 };
-	right = permution(right,48,ED);
 
-	right = XOR(key,right);
+	right = permution(right,48,ED,32);
+	printf("ed_box: %016llX\n", right);
+	right = key ^ right;
+	//printf("xor: %016llX\n", right);
 	
 	return right;
 }
-string D_box(string a) {
+u64 D_box(u64 a) {
 	int per[32] = { 16, 7, 20, 21,
 				   29, 12, 28, 17,
 				   1, 15, 23, 26,
@@ -312,102 +277,98 @@ string D_box(string a) {
 				   19, 13, 30, 6,
 				   22, 11, 4, 25 };
 
-	a = permution(a,32,per);
-
+	a = permution(a,32,per,32);
+	//printf("d-box: %016llX\n", a);
 	return a;
 }
 
 
-string S_box(string a) 
+
+
+u64 S_box(u64 a)
 {
-	string b = "";
-	int s[8][4][16] = { { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+	int s [ ]= { 14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
 						  0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
 						  4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
-						  15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 },
-						{ 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+						  15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13 ,
+						 15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
 						  3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
 						  0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
-						  13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 },
+						  13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9 ,
 
-						{ 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+						 10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
 						  13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
 						  13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
-						  1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 },
-						{ 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+						  1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12 ,
+					 7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
 						  13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
 						  10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
-						  3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 },
-						{ 2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+						  3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14 ,
+						2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
 						  14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
 						  4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
-						  11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 },
-						{ 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+						  11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3 ,
+						 12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
 						  10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
 						  9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
-						  4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 },
-						{ 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+						  4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13 ,
+						 4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
 						  13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
 						  1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
-						  6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 },
-						{ 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+						  6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12 ,
+						 13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
 						  1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
 						  7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
-						  2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 } };
-
-	
+						  2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11 };
+	u64 val = 0;
+	int* box ;
 	for (int i = 0; i < 8; i++) {
-		//row depend on first and last bits 
-		int row = 2 * int(a[i * 6] - '0') + int(a[i * 6 + 5] - '0');
-		//column depend on the middle bits
-		int column = 8 * int(a[i * 6 + 1] - '0') + 4 * int(a[i * 6 + 2] - '0') + 2 * int(a[i * 6 + 3] - '0') + int(a[i * 6 + 4] - '0');
-		int val = s[i][row][column];
-		//from decimal int to binary string
-		b += char(val / 8 + '0');
-		val = val % 8;
-		b += char(val / 4 + '0');
-		val = val % 4;
-		b += char(val / 2 + '0');
-		val = val % 2;
-		b += char(val + '0');
+		
+		box = s + (i * 64);
+		u64 idx = a >> (7 - i) * 6 & 0x3F;//get the index
+		
+		idx = idx >> 1 & 15 | (idx & 1) << 4 | idx & 0x20;
+		val |=(u64) box[idx ]<< (7 - i) * 4;
+		
 	}
-
-	return b ;
+	
+	return val;
 }
-
-
-
-
-
-void Round(string key)
+//right_p is used
+void Round(u64 key)
 {
-	string test;
+	u64 test;
 	test = ED_box(Right_p,key);
-
+	//printf("ed_box: %016llX\n", test);
+	
 	test = S_box(test);
 
 	test = D_box (test);
 
-	Left_p = XOR(test, Left_p);
-
+	Left_p = test ^ Left_p;
 
 }
 
-string encrypt(string key , string text)
+u64 encrypt(u64 key , u64 text)
 {
 
 	//intial permutation
-	text = permution(text, 64, intialper);
-	//cout << binary_to_hexa(text)<<endl;
-	string x = text;
-	Left_p = text.substr(0, 32);
-	Right_p= text.substr(32, 32);
+	text = permution(text, 64, intialper,64);
+	u64 x = text;
+
+
+
+
+	Left_p = (text >> 32) & 0xffffffff;
+	Right_p= text & 0xffffffff;
+	
 	for (int i = 0;i < 16;i++)
 	{
-		//x = Round(keys[i],x,i);
-		Round(keys[i]);
-		string left = Left_p;
-		string right = Right_p;
+		
+
+		Round(keys_64[i]);
+		u64 left = Left_p;
+		u64 right = Right_p;
 
 		if (i != 15)
 		{
@@ -415,47 +376,74 @@ string encrypt(string key , string text)
 			Left_p = right;
 		}
 	}
-	text = Left_p + Right_p;
-	text = permution(text, 64, final_perm);
+	
+	//text = Left_p + Right_p;
+	text =( Left_p << 32) | Right_p;
+
+	text = permution(text, 64, final_perm,64);
 	return text;
 }
 
 
-int main()
+int main()//int argc, char** argv)
 {
-	string type, key, planetext;
-
+//if (argc == 4)
+	//{
+	string type;
+	string key1, planetext1;
+	u64 key, planetext;
 	cin >> type;
-	cin >> planetext;
-	cin >> key;
-
-	key = hexa_to_binary(key);
-	planetext = hexa_to_binary(planetext);
-
-	if (type == "encrypt" || type == "Encrypt")
-	{
-		key_generate(key);
-		planetext = encrypt(key,planetext);
-		//print output
-		planetext = binary_to_hexa(planetext);
-		cout << "cipher: " + planetext << endl;
-	}
-	else if (type == "decrypt" || type == "Decrypt")
-	{
+	cin >> planetext1 ;
+	cin >>key1;
+	
 		
-		key_generate_(key);
+		key1 = hexa_to_binary(key1); //for the key generation function
+		
 
-		planetext =  encrypt(key, planetext);
-		//print output
-		planetext = binary_to_hexa(planetext);
-		cout << "plain: " + planetext << endl;
+		//decimal output
+		planetext = stoull(planetext1,nullptr,16);
+		key = stoull(key1, nullptr, 2);
+		
 
-	}
-	else cout << "Invalid operation";
+		
+
+		if (type == "encrypt" || type == "Encrypt")
+		{
+			key_generate(key1);
+			
+
+			long long t1 = __rdtsc();
+			
+			planetext = encrypt(key, planetext);
+			
+			long long t2 = __rdtsc();
+
+			printf("cipher: %016llX\n", planetext);
+			
+			printf("Cycles: %lld\n", t2 - t1);
+		
+		}
+		else if (type == "decrypt" || type == "Decrypt")
+		{
+
+			key_generate_(key1);
+
+		
+			long long t1 = __rdtsc();
+
+			planetext = encrypt(key, planetext);
+
+			long long t2 = __rdtsc();
+			
+			printf("plain: %016llX\n", planetext);
+			printf("Cycles: %lld\n", t2 - t1);
+		
+
+		}
+		else cout << "Invalid operation";
 
 
 
-
-
-
+		return 0;
+	
 }
